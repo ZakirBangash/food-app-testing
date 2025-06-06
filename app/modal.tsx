e23a6d61d38d187640b746a33c0b4e0da43cc166
Zakir Bangash <66157395+ZakirBangash@users.noreply.github.com>
2025-06-06 22:04:03 +0500

    init

--- app/modal.tsx
+++ app/modal.tsx
@@ -3,6 +3,7 @@ import { Platform, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Stack } from 'expo-router';
 
 export default function ModalScreen() {
   return (
